cc462a81e5acb0d745215ed6f3ea700528ec1082
Mattis Stordalen Flister <stordalen@gmail.com>
2010-03-25 23:40:47 +0100

    Added a transmission daemon controller

---
+++ lib/TransmissionConnector/transmission_daemon_controller.rb
@@ -0,0 +1,29 @@
+require 'daemons'
+
+module TransmissionConnector
+  class TransmissionDaemonController
+    def initialize(config = "")
+      config_dir = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", 'spec', 'support', 'config_dir')
+      @config_arg = "--config-dir #{config_dir}"
+    end
+    
+    def start
+      @transmission_daemon = Daemons.call(:backtrace => true) do
+        exec "/usr/bin/transmission-daemon --foreground #{@config_arg}"
+      end      
+    end
+    
+    def stop
+      @transmission_daemon.stop
+    end
+    
+    def show_status
+      @transmission_daemon.show_status
+    end
+  end
+end
+
+cont = TransmissionConnector::TransmissionDaemonController.new
+cont.start
+sleep 60
+cont.stop
--- spec/authenticator_spec.rb
+++ spec/authenticator_spec.rb
@@ -3,19 +3,12 @@ require 'daemons'
 
 describe "connection mechanism" do
   it "should be able to authenticate to a running transmission daemon" do
-    
+    start_transmission
   end
   
   def start_transmission
-    @transmission = Daemons.call do
-      system "transmission-daemon --config-dir /home/mattis/Ruby/workspace-radrails/TorrentHub/spec/transmission/config_dir/ --foreground"      
-    end    
-    
-    
-    # wait for "Transmission 1.75 (9117) started"
   end
   
   def stop_transmission
-    @transmission.stop
   end  
 end
