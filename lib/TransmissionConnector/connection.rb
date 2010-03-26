020c8ee1b61e6c8217d0f26a8af5e387606cd372
Mattis Stordalen Flister <mattis.stordalen.flister@gmail.com>
2010-03-26 23:58:48 +0100

    Created a spec to check that the connection can get itself a session id

--- lib/TransmissionConnector/connection.rb
+++ lib/TransmissionConnector/connection.rb
@@ -3,25 +3,23 @@ module TransmissionConnector
     require 'uri'
     require 'httpclient'
     
+    attr_reader :session_id
+    
     def initialize(username, password, host)
-      @transmission_host = host 
-      
-      url = URI.parse(@transmission_host)
+      @rpc_url = "#{host}/transmission/rpc"
       
       @client = HTTPClient.new
-      @client.set_auth(url, username, password)
+      @client.set_auth(URI.parse(host), username, password)
+      
+      @session_id_header_key = 'X-Transmission-Session-Id'
+    end
+    
+    def set_session_id
+        @session_id = @client.post(@rpc_url).header[@session_id_header_key].pop
     end
     
     def post_to_rpc(data)
-      rpc_url = @transmission_host + '/transmission/rpc'
-      session_id_header_key = 'X-Transmission-Session-Id'
-      
-      if @sessionId.nil? then
-        @sessionId = @client.post(rpc_url).header[session_id_header_key]
-        post_to_rpc(data)
-      else
-        JSON::parse(@client.post(rpc_url, data.to_json, session_id_header_key => @sessionId).content)
-      end
+      JSON::parse(@client.post(@rpc_url, data.to_json, @session_id_header_key => @session_id).content)
     end    
   end
 end
--- spec/authenticator_spec.rb
+++ spec/authenticator_spec.rb
@@ -6,17 +6,28 @@ require 'daemons'
 
 describe TransmissionConnector::Connection, "connection mechanism" do
   before(:all) do
+    @host = '127.0.0.1'
+    @port = 13337
+    
     config_dir = File.expand_path('../support/config_dir', __FILE__)
     @transmission_daemon = TransmissionConnector::DaemonController.new(
       :config_dir => config_dir, 
-      :host       => '127.0.0.1', 
-      :port       => 13337
+      :host       => @host,
+      :port       => @port
     )
     
     @transmission_daemon.start_and_wait
   end
   
   it "should be able to authenticate to a running transmission daemon" do
+    #given
+    connection = TransmissionConnector::Connection.new('mattis', 'mattis', "http://#{@host}:#{@port}")
+    
+    #when
+    connection.set_session_id
+    
+    #then
+    connection.session_id.should_not be_nil
   end
   
   after(:all) do
