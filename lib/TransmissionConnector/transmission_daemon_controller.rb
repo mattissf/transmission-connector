6f3d835233a781078d712ea0e4c6364018518fc0
Mattis Stordalen Flister <mattis.stordalen.flister@gmail.com>
2010-03-26 23:40:03 +0100

    Now successfully starts a transmission daemon, waits for it to be ready to accept connections, and continues tests

--- lib/TransmissionConnector/transmission_daemon_controller.rb
+++ lib/TransmissionConnector/transmission_daemon_controller.rb
@@ -1,24 +1,67 @@
-require 'daemons'
-
 module TransmissionConnector
-  class TransmissionDaemonController
-    def initialize(config = "")
-      config_dir = File.expand_path('../../../spec/support/config_dir', __FILE__)
-      @config_arg = "--config-dir #{config_dir}"
+  class DaemonController
+    def initialize(parameters)
+      @config_dir = parameters[:config_dir]
+      @host       = parameters[:host]
+      @port       = parameters[:port]
     end
     
     def start
-      @transmission_daemon = Daemons.call(:backtrace => true) do
-        exec "/usr/bin/transmission-daemon --foreground #{@config_arg}"
-      end      
+      @pid = fork
+      
+      if(@pid)
+        puts "Spawned process with pid #{@pid}"
+      else
+        exec "/usr/bin/transmission-daemon --foreground --config-dir #{@config_dir} --port #{@port}"
+      end
+    end
+    
+    def start_and_wait
+      start
+      wait_for_transmission
+    end
+    
+    def wait_for_transmission(max_wait_time = 30)
+      start = Time.new.to_i
+      
+      while(Time.new.to_i < (start + max_wait_time))
+        puts "Waiting for transmission to get ready..."
+        begin
+          Timeout::timeout(1) do
+            begin
+              socket = TCPSocket.new(@host, @port)
+              socket.close
+              return true #Up and running, continue program
+            rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+              # Probably not up, continue loop
+              sleep 0.2
+            end
+          end
+        rescue Timeout::Error
+          # One second timeout, continue loop
+        end        
+      end
+      
+      #If code ends up here then it's because we never got a connection
+      raise "Transmission daemon did not start in #{max_wait_time} seconds"
     end
     
     def stop
-      @transmission_daemon.stop
+      Process.kill('TERM', @pid)
+      Process.waitpid(@pid)
     end
     
     def show_status
-      @transmission_daemon.show_status
+      begin
+        Process.kill(0, @pid)
+        puts "#{@pid} is alive."
+      rescue Errno::EPERM
+        puts "#{@pid} has detached.";
+      rescue Errno::ESRCH
+        puts "#{@pid} is dead.";
+      rescue
+        puts "Status of #{@pid} unavailable : #{$!}"
+      end      
     end
   end
-end 
+end
--- spec/authenticator_spec.rb
+++ spec/authenticator_spec.rb
@@ -1,14 +1,25 @@
+require File.expand_path('../../lib/TransmissionConnector/connection', __FILE__)
+require File.expand_path('../../lib/TransmissionConnector/transmission_daemon_controller', __FILE__)
+
 require File.dirname(__FILE__) + '/spec_helper.rb'
 require 'daemons'
 
-describe "connection mechanism" do
+describe TransmissionConnector::Connection, "connection mechanism" do
+  before(:all) do
+    config_dir = File.expand_path('../support/config_dir', __FILE__)
+    @transmission_daemon = TransmissionConnector::DaemonController.new(
+      :config_dir => config_dir, 
+      :host       => '127.0.0.1', 
+      :port       => 13337
+    )
+    
+    @transmission_daemon.start_and_wait
+  end
+  
   it "should be able to authenticate to a running transmission daemon" do
-    start_transmission
   end
   
-  def start_transmission
+  after(:all) do
+    @transmission_daemon.stop
   end
-  
-  def stop_transmission
-  end  
 end
